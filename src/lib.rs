b4c3a14157fbeb6188f094bab5f8f017c3ad9956
firelight flagboy <firelight.flagboy@gmail.com>
2023-10-04 15:30:40 +0200

    Support `bash-command` mode
    Allow to execute bash command to retrieve the pkg's latest version.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,6 @@ pub mod pkg_info;
 mod serde_utils;
 
 pub use pkg_info::{
-    Arch, Base as PkgInfoBase, Digest, GithubReleaseHandler, Mode as PkgInfoMode,
-    ModeGetLatestVersion, PkgInfo, VersionedArchEntry,
+    Arch, Base as PkgInfoBase, BashCmdReleaseHandler, Digest, GithubReleaseHandler,
+    Mode as PkgInfoMode, ModeGetLatestVersion, PkgInfo, VersionedArchEntry,
 };
