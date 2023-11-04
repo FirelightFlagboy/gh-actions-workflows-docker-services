19e90a21985fc770112ba6f876e56c8e1d7a5884
FirelightFlagboy <30697622+FirelightFlagboy@users.noreply.github.com>
2023-11-04 10:07:31 +0100

    Rework release script (#26)
    * Rework release script
    
    * Allow to skip the release part
    
    * Extract note for release from unreleased changelog
    
    * Update changelog
    
    - Use `UNRELEASED-CHANGELOG` to trace yet to release change.
    - On a release the content of `UNRELEASED-CHANGELOG` is moved to
      `CHANGELOG`.

--- script/make-release.sh
+++ script/make-release.sh
@@ -2,42 +2,112 @@ set -eu -o pipefail
 
 SCRIPTDIR=${SCRIPTDIR:=$(dirname $(realpath -s "$0"))}
 ROOTDIR=${ROOTDIR:=$(realpath -s "$SCRIPTDIR/../../..")}
+SKIP_RELEASE_CREATION=${SKIP_RELEASE_CREATION:-0}
 
 NEW_VERSION=${1:?Missing new version}
+RELEASE_DATE=$(date --rfc-3339=date --utc)
 
-sed -i "s/^version = \".*\"$/version = \"$NEW_VERSION\"/" Cargo.toml
+TEMP_FILES=()
 
-cargo build --release
+function cleanup_temp_files {
+  for temp_file in "${TEMP_FILES[@]}" ; do
+    rm $temp_file
+  done
+}
 
-ARCH=amd64
-OS=linux
+trap "cleanup_temp_files" EXIT INT
 
-SHA512SUM=$(sha512sum target/release/pkg-info-updater | cut -f 1 -d' ')
+function build_new_version_for_bin {
+  sed -i "s/^version = \".*\"$/version = \"$NEW_VERSION\"/" Cargo.toml
 
-FILENAME=pkg-info-updater-$OS-$ARCH
-DOWNLOAD_URL=https://github.com/FirelightFlagboy/gh-actions-workflows-docker-services/releases/download/v$NEW_VERSION/pkg-info-updater-linux-amd64
-TEMP_FILE=$(mktemp)
-trap "[ -f $TEMP_FILE ] && rm -f $TEMP_FILE" EXIT INT
+  cargo build --release
+}
 
-jq \
-  ".versions[\"$NEW_VERSION\"] = {\"$ARCH\":{\"filename\":\"$FILENAME\",\"download_url\":\"$DOWNLOAD_URL\",\"digest\":\"sha512:$SHA512SUM\"}} | .latest_version = \"$NEW_VERSION\"" \
-  pkg-info.json > $TEMP_FILE
+function add_new_version_to_pkg_file {
+  local ARCH=amd64
+  local OS=linux
+  local FILENAME=pkg-info-updater-$OS-$ARCH
+  local DOWNLOAD_URL=https://github.com/FirelightFlagboy/gh-actions-workflows-docker-services/releases/download/v$NEW_VERSION/pkg-info-updater-linux-amd64
+  local SHA512SUM=$(sha512sum target/release/pkg-info-updater | cut -f 1 -d' ')
+  local TEMP_FILE=$(mktemp)
+  TEMP_FILES+=($TEMP_FILE)
 
-mv $TEMP_FILE pkg-info.json
+  jq \
+    ".versions[\"$NEW_VERSION\"] = {\"$ARCH\":{\"filename\":\"$FILENAME\",\"download_url\":\"$DOWNLOAD_URL\",\"digest\":\"sha512:$SHA512SUM\"}} | .latest_version = \"$NEW_VERSION\"" \
+    pkg-info.json > $TEMP_FILE
 
-git add pkg-info.json Cargo.toml Cargo.lock
+  cp $TEMP_FILE pkg-info.json
+}
 
-git commit --signoff --gpg-sign -m "Prepare for release $NEW_VERSION"
-git tag --sign "v$NEW_VERSION" --message="Release version $NEW_VERSION"
+function changelog_for_release {
+  grep -v '^<!-- markdownlint-configure-file .* -->$' UNRELEASED-CHANGELOG.md
+}
 
-git push --atomic origin main "v$NEW_VERSION"
+function update_changelog {
+  local TEMP_CHANGELOG=$(mktemp)
+  TEMP_FILES+=($TEMP_CHANGELOG)
+  (
+    echo
+    echo "## $NEW_VERSION ($RELEASE_DATE)"
+    echo
+    changelog_for_release
+  ) > $TEMP_CHANGELOG
 
-cp target/release/pkg-info-updater /tmp/pkg-info-updater-$OS-$ARCH
+  # Include changelog entry after marker
+  sed -i "/split-marker/ r $TEMP_CHANGELOG" CHANGELOG.md
+  # Remove content from UNRELEASED-CHANGELOG
+  sed -i '/markdownlint-configure-file/!d' UNRELEASED-CHANGELOG.md
+}
 
-gh release create "v$NEW_VERSION" \
-  --title "$NEW_VERSION" \
-  --draft \
-  --generate-notes \
-  --latest \
-  --verify-tag \
-  "/tmp/pkg-info-updater-$OS-$ARCH"
+function commit_file_for_release {
+  git add pkg-info.json Cargo.toml Cargo.lock CHANGELOG.md UNRELEASE-CHANGELOG.md
+  git commit --signoff --gpg-sign -m "Prepare for release $NEW_VERSION"
+}
+
+function create_tag {
+  git tag --sign "v$NEW_VERSION" --message="Release version $NEW_VERSION"
+}
+
+function push_change {
+  git push --atomic origin main "v$NEW_VERSION"
+}
+
+function create_release_with_artifact {
+  cp target/release/pkg-info-updater /tmp/pkg-info-updater-$OS-$ARCH
+
+  gh release create "v$NEW_VERSION" \
+    --title "$NEW_VERSION" \
+    --draft \
+    # --generate-notes \
+    --notes-file $1 \
+    --latest \
+    --verify-tag \
+    "/tmp/pkg-info-updater-$OS-$ARCH"
+}
+
+build_new_version_for_bin
+
+add_new_version_to_pkg_file
+
+RELEASE_BLOB=$(mktemp)
+TEMP_FILES+=($RELEASE_BLOB)
+
+changelog_for_release > $RELEASE_BLOB
+
+update_changelog
+
+if [ $SKIP_RELEASE_CREATION -ne 0 ]; then
+  echo "SKIP_RELEASE_CREATION set, skipping release creation"
+  echo
+  echo -e "\x1b[1mThe release would have used this notes:\x1b[0m"
+  cat $RELEASE_BLOB
+  exit 0
+fi
+
+commit_file_for_release
+
+create_tag
+
+push_change
+
+create_release_with_artifact $RELEASE_BLOB
